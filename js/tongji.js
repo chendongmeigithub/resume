7f9401c138ccf3adbb74460961b3e77a35dfa946
chendongmei <1174411878@q.com>
2018-12-21 15:12:46 +0800

    [add] 添加代码统计与图片压缩

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0c214e4fe2bf552369ad31a735fb204b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
